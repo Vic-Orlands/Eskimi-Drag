3005e39370cffcc72d25179b60cc87e82fdf2366
Vic.Orlands <chimezieinnocent39@gmail.com>
2020-11-17 23:06:00 +0100

    Complete Landing page

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "../styles/About.css"
+import Nav from './Nav';
+
+const About = () => {
+    return (
+        <div>
+            <Nav />
+            hello
+        </div>
+    );
+};
+
+export default About;
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,81 +1,28 @@
 import React, { Component } from 'react';
 import '../styles/Homepage.css';
-import Modal from './Modal';
+import Nav from './Nav';
 
-import { RiAppleFill } from 'react-icons/ri';
+import { BsArrowRight } from 'react-icons/bs';
 
 class Homepage extends Component {
-	state = {
-		open: true
-	};
-
-	openModal = (e) => {
-		e.preventDefault();
-		this.setState({
-			open: !this.state.open
-		});
-	};
-
 	render() {
 		return (
-			<main>
-				<section className={'top-section ' + (!this.state.open ? 'modal-overlay' : '')}>
-					<div className="intro-wrapper">
-						<img src={require('../assets/logo.png')} alt="logo_img" />
+			<section className="section">
+				<Nav />
 
-						<h1>
-							Know exactly where your money goes and
-							<span> manage</span> your expenses better.
-						</h1>
+				<header>
+					<hgroup>
+						<h4>#1 interior decoration in nigeria</h4>
+						<h1>focus on</h1>
+						<h1>design quality</h1>
+					</hgroup>
+					<p>we create lasting impression through interior designs.</p>
 
-						<h3>Our app let's you automatically track your expenses and budgets.</h3>
-						<h3>Let's help you be on top of your finances. Fill the form below and we</h3>
-						<h3>could grant you early VIP access.</h3>
-
-						<button
-							onClick={(e) => {
-								this.openModal(e);
-							}}
-						>
-							Notify me
-						</button>
-					</div>
-				</section>
-
-				<section className={'bottom-section ' + (!this.state.open ? 'modal-overlay-bottom' : '')}>
-					<img className="overlap-img imgOne" src={require('../assets/Dashboard.png')} alt="phone_img" />
-
-					<div className="playstore">
-						<h3>Coming Soon on</h3>
-
-						<div className="stores">
-							<div className="android">
-								<img
-									className="playstore_img"
-									src={require('../assets/playstore.jpg')}
-									alt="phone_img"
-								/>
-								<div>
-									<h4>Android APP ON</h4>
-									<h2>Google play</h2>
-								</div>
-							</div>
-
-							<div className="ios">
-								<RiAppleFill id="font" />
-								<div>
-									<h4>Download on the</h4>
-									<h2>App Store </h2>
-								</div>
-							</div>
-						</div>
-					</div>
-
-					<img className="overlap-img imgTwo" src={require('../assets/Expenses.png')} alt="phone_img" />
-				</section>
-
-				<Modal open={this.state.open} onClose={this.openModal} />
-			</main>
+					<a>
+						<BsArrowRight id="font" />contact us
+					</a>
+				</header>
+			</section>
 		);
 	}
 }
